11443bc2dfa3d3440492b33fe79a1a3fec62a7ca
Coding-Altair <fidel.jotojot@csucc.edu.ph>
2025-07-12 23:51:15 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Timbershoppe</title>
+    <link rel="icon" href="/Logo.svg" type="image/svg+xml">
+    <link rel="stylesheet" href="../css/app.css">
+    <script src="https://kit.fontawesome.com/0959c857e5.js" crossorigin="anonymous"></script>
     @vite(['resources/js/app.js'])
 </head>
 <body>
