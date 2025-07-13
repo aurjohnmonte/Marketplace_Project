d59dafdf56a7eba6d2b80d6976864b17d23247dd
Coding-Altair <fidel.jotojot@csucc.edu.ph>
2025-07-13 22:35:32 +0800

    Create ShopSignup.vue

---
+++ resources/js/forms/ShopSignup.vue
@@ -0,0 +1,20 @@
+<template>
+    <form method="POST" class="form-content">
+        <div class="form-header">
+            <h1>Shop up</h1>
+            <p>Sign up as a <strong>{{ userType }}</strong></p>
+        </div>
+
+        <button class="form-btn" id="next-btn">Next</button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            userType: 'seller'
+        }
+    }
+}
+</script>
