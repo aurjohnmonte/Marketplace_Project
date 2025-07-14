4562b78403f2f0ab9790ed09d33f84ec0d43d4ed
Coding-Altair <fidel.jotojot@csucc.edu.ph>
2025-07-15 00:40:33 +0800

    Update App.vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -4,8 +4,8 @@
     <nav class="navbar navbar-light bg-light">
         <h1>Timbershoppe</h1>
         <ul>
-            <li><button class="btn" @click="action = 'signup'">Sign Up</button></li>
-            <li><button class="btn" @click="action = 'login'">Login</button></li>
+            <li><button class="btn style" @click="action = 'signup'">Sign Up</button></li>
+            <li><button class="btn style" @click="action = 'login'">Login</button></li>
         </ul>
     </nav>
 
@@ -43,7 +43,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 .container {
   width: 100%;
   min-width: 320px;
@@ -78,11 +78,83 @@ export default {
   display: flex;
   flex: 1;
   align-items: center;
-  gap: 15em;
+  justify-content: center;
   padding: 0 2rem;
   overflow: auto;
   position: relative;
   z-index: 1;
   font-family: 'Roboto-Reg';
+  gap: 10em;
+}
+
+/* Responsive Design */
+@media (max-width: 1200px) {
+  .content {
+    gap: 5em;
+  }
+}
+
+@media (max-width: 900px) {
+  .content {
+    flex-direction: column;
+    align-items: center;
+    gap: 2em;
+    padding: 1rem;
+  }
+  .content img {
+    width: 160px;
+    margin-bottom: 1.5rem;
+    max-width: 60vw;
+    height: auto;
+  }
+  .signup-container, .login-container {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 auto;
+  }
+}
+
+@media (max-width: 600px) {
+  .container {
+    min-width: 0;
+    padding: 0;
+  }
+  .navbar {
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 1rem 1rem 0;
+  }
+  .navbar h1 {
+    font-size: 1.2rem;
+    margin-bottom: 0.5rem;
+    word-break: break-word;
+  }
+  .navbar ul {
+    margin: 0 0 0.5rem 0;
+    padding: 0;
+  }
+  .navbar ul li {
+    margin-right: 1rem;
+  }
+  .content {
+    flex-direction: column;
+    gap: 1em;
+    padding: 0.5rem;
+    align-items: stretch;
+  }
+  .content img {
+    width: 90px;
+    margin-bottom: 1rem;
+    max-width: 80vw;
+    height: auto;
+  }
+  .signup-container, .login-container {
+    max-width: 100%;
+    padding: 0 0.5rem;
+  }
+  .navbar ul li {
+    display: block;
+    margin: 0 0 0.5rem 0;
+  }
 }
 </style>
