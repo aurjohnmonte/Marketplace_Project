95a2ece59ee8b043faab166a0af84381a4ffa792
Coding-Altair <fidel.jotojot@csucc.edu.ph>
2025-07-15 17:10:02 +0800

    Update Login.vue

--- resources/js/forms/Login.vue
+++ resources/js/forms/Login.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="form-content">
-        <form method="POST">
+        <form method="POST" @submit.prevent="handleSubmit">
             <h1>LOGIN</h1>
             <div
                 v-for="field in requiredInfo"
@@ -9,12 +9,16 @@
             >
             <div class="input-box">
                 <input
+                v-model="data[field.id]"
                 :type="field.type"
                 :name="field.id"
                 :id="field.id"
                 required
+                @blur="validateField(field.id)"
+                @input="validateField(field.id)"
                 />
                 <label :for="field.id">{{ field.label }}</label>
+                <div v-if="errors[field.id]" class="error-message">{{ errors[field.id] }}</div>
             </div>
             </div>
             <button
@@ -36,6 +40,39 @@ export default {
             data: {
                 username: '',
                 password: ''
+            },
+            errors: {}
+        }
+    },
+    methods: {
+        validateField(field) {
+            if (field === 'username') {
+                if (!this.data.username) {
+                    this.errors.username = 'Username is required.';
+                } else if (this.data.username.length < 3) {
+                    this.errors.username = 'Username must be at least 3 characters.';
+                } else {
+                    delete this.errors.username;
+                }
+            }
+            if (field === 'password') {
+                if (!this.data.password) {
+                    this.errors.password = 'Password is required.';
+                } else if (this.data.password.length < 6) {
+                    this.errors.password = 'Password must be at least 6 characters.';
+                } else {
+                    delete this.errors.password;
+                }
+            }
+        },
+        validateForm() {
+            this.validateField('username');
+            this.validateField('password');
+            return Object.keys(this.errors).length === 0;
+        },
+        handleSubmit() {
+            if (this.validateForm()) {
+                alert('Login successful! (Validation passed)');
             }
         }
     }
@@ -48,11 +85,15 @@ export default {
   background-color:	#F5F5DC;
   display: flex;
   flex-direction: column;
-  align-items: center;
   width: 30%;
-  margin-left: 8rem;
+  min-width: 320px;
+  max-width: 600px;
+  margin: 0 auto;
+  height: auto;
   border: 1px solid #000;
   border-radius: 10px;
+  border-radius: 10px;
+  box-sizing: border-box;
 }
 
 .form-content{
@@ -161,5 +202,10 @@ export default {
 .form-btn {
   padding: 8px;
   border-radius: 10px;
+}
+.error-message {
+  color: #b00020;
+  font-size: 12px;
+  margin-top: 2px;
 }
 </style>
