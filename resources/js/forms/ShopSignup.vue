595bcf428d7d8309868632bf0fdcb8607d8e56ad
Coding-Altair <fidel.jotojot@csucc.edu.ph>
2025-07-16 17:15:00 +0800

    Update ShopSignup.vue

--- resources/js/forms/ShopSignup.vue
+++ resources/js/forms/ShopSignup.vue
@@ -1,11 +1,24 @@
 <template>
     <form method="POST" class="form-content">
         <div class="form-header">
-            <h1>Shop up</h1>
-            <p>Sign up as a <strong>{{ userType }}</strong></p>
+            <h1>Sign up</h1>
+            <p>Shop Details</p>
         </div>
 
-        <button class="form-btn" id="next-btn">Next</button>
+        <div>
+            <div v-for="details in shopInfo" :key="details.id" class="input-row">
+                <div class="input-box">
+                    <input
+                        :type="details.type"
+                        :name="details.name"
+                        :id="details.id"
+                    >
+                    <label :for="details.name">{{ details.label }}</label>
+                </div>
+
+            </div>
+        </div>
+        <button class="form-btn style" id="next-btn">Next</button>
     </form>
 </template>
 
@@ -13,7 +26,13 @@
 export default {
     data() {
         return {
-            userType: 'seller'
+            shopInfo: [
+                {label: 'Shop Name', type: 'text', name: 'shopName', id: 'shopName' },
+                {label: 'Shop Category', type: 'text', name: 'shopCategory', id: 'shopCategory' },
+                {label: 'Shop Description', type: 'text', name: 'shopDescription', id: 'shopDescription' },
+                {label: 'Shop Address', type: 'text', name: 'shopAddress', id: 'shopAddress' },
+                {label: 'Upload Shop Image', type: 'file', name: 'shopProfile', id: 'shopProfile' },
+            ]
         }
     }
 }
