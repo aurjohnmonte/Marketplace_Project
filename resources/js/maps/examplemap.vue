3c3f6907e5e5a5e16765bc4e580d20ab4b8db191
aurjohnmonte <aurjohnmonte2003@gmail.com>
2025-10-18 19:47:56 +0800

    This is the latest code

--- resources/js/maps/examplemap.vue
+++ resources/js/maps/examplemap.vue
@@ -12,6 +12,10 @@ import L from "leaflet";
 import { watch } from "vue";
 import { useDataStore } from "../stores/dataStore";
 
+import 'leaflet/dist/leaflet.css'
+import 'leaflet-routing-machine'
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'
+
 export default {
     props: ['shops'],
     data(){
@@ -19,27 +23,58 @@ export default {
             map_object: null,
             user_marker: null,
             user_circle: null,
+            watchID: null,
             completed_load: false,
             store: useDataStore(),
         }
     },
     methods: {
-        getLocation() {
+        getLocation(lat, long) {
+
+            console.log(`lat: ${lat}, long: ${long}`);
+
             if (!navigator.geolocation) {
                 alert("Geolocation is not supported by your browser.")
                 return
             }
 
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                const lat = position.coords.latitude
-                const lng = position.coords.longitude
+            //i need to clearWatch because it need to stop the previous watchPosition before starting again to avoid error.
+            navigator.geolocation.clearWatch(this.watchID);
 
-                this.mapuserLoc(lat, lng);
+            let routingControl;
+
+            this.watchID = navigator.geolocation.watchPosition(
+                (position) => {
+                    console.log('hello');
+                    
+                    this.store.setUser_location(position.coords.latitude, position.coords.longitude);
+
+                    if (routingControl) {
+
+                        routingControl.setWaypoints([userLatLng, destination]);
+                    } 
+                    else {
+                        
+                        routingControl = L.Routing.control({
+                            waypoints: [userLatLng, destination],
+                            routeWhileDragging: false,
+                            addWaypoints: false,
+                            show: false,
+                        }).addTo(map);
+                    }
+
+                    console.log('user location: ',this.store.user_location);
                 },
                 (error) => {
-                alert("Unable to retrieve your location. You may have denied permission.")
-                console.error(error)
+
+                    alert("Unable to retrieve your location. You may have denied permission.")
+                    console.error(error)
+                },
+                {
+
+                    enableHighAccuracy: true,
+                    maximumAge: 1000,     // Accept cached position max 1 second old
+                    timeout: 10000        // Timeout for getting position
                 }
             )
         },
@@ -94,20 +129,43 @@ export default {
             this.getLocation();
         },
 
+        goNavigateShopLocation(lat, long){
+
+            console.log('go navigate');
+            this.getLocation(lat, long);
+        },
+
         viewShopLocation(){
             this.shops.forEach(shop => {
 
 
                 L.marker([shop.latitude, shop.longitude])
                     .addTo(this.map_object)
-                    .bindPopup(`<strong style='color: #D25E27;'>${shop.name}</strong><br><label style='font-size: 12px;'>${shop.address}</label><br><br><button id="go-btn">Visit this shop</button>`)
-                    .on('popupopen', ()=> {
+                    .bindPopup(`
+                    <strong style='color: #D25E27;'>${shop.name}</strong><br>
+                    <label style='font-size: 12px;'>${shop.address}</label>
+                    <br><br>
+                    <button id="go-btn" style="margin-bottom: 5px;">Visit this shop</button><br>
+                    <button id="navigate-btn">Navigate</button>
+                    `)
+                    .on('popupopen', () => {
 
                         const btn = document.getElementById("go-btn");
+                        const navigate_btn = document.getElementById("navigate-btn");
+
                         if (btn) {
                             btn.addEventListener("click", () => {
                                 console.log('clicked');
                                 this.$router.push({name: 'ShopAbout', params: {id: shop.id}});
+                            });
+                        }
+
+                        //for navigation
+                        if(navigate_btn){
+                            navigate_btn.addEventListener('click', () => {
+
+                                console.log('shop: ',shop);
+                                this.goNavigateShopLocation(shop.latitude, shop.longitude);
                             });
                         }
                     });
@@ -149,7 +207,13 @@ export default {
         //https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
         //tiles sa sattelite ver.
 
-        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+        // L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+        //     attribution: 'Tiles &copy; Esri — Sources: Esri, Garmin, USGS, etc.',
+        //     maxZoom: 19,
+        //     minZoom: 3,
+        // }).addTo(this.map_object);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Tiles &copy; Esri — Sources: Esri, Garmin, USGS, etc.',
             maxZoom: 19,
             minZoom: 3,
@@ -197,6 +261,7 @@ export default {
             longitude: null,
         };
         store.resetSelectedCoordinate();
+        this.watchID = null;
     }
 }
 </script>
